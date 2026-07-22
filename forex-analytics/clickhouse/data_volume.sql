67583adf11998a6cacdf912dec39534ea1316bc6
Sergei Berezin <skazka064@mail.ru>
2026-07-22 17:35:19 +0300

    Update data_volume.sql

--- forex-analytics/clickhouse/data_volume.sql
+++ forex-analytics/clickhouse/data_volume.sql
@@ -1,1 +1,17 @@
+## Проверка количества данных
 
+SELECT 
+    'ticks (Airflow)' AS table_name,
+    COUNT(*) AS rows,
+    MIN(timestamp) AS first,
+    MAX(timestamp) AS last
+FROM forex_data.ticks
+
+UNION ALL
+
+SELECT 
+    'ticks_kafka (Kafka)' AS table_name,
+    COUNT(*) AS rows,
+    MIN(timestamp) AS first,
+    MAX(timestamp) AS last
+FROM forex_data.ticks_kafka;

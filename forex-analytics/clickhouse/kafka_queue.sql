7737a026779653ac11ea90e66086815ddaad632b
Sergei Berezin <skazka064@mail.ru>
2026-07-22 17:32:44 +0300

    Create kafka_queue.sql

---
+++ forex-analytics/clickhouse/kafka_queue.sql
@@ -0,0 +1,15 @@
+## Очередь для Kafka
+
+CREATE TABLE forex_data.kafka_queue
+(
+    symbol String,
+    bid Float64,
+    ask Float64,
+    timestamp DateTime
+)
+ENGINE = Kafka()
+SETTINGS 
+    kafka_broker_list = '10.0.0.21:9092',
+    kafka_topic_list = 'forex_ticks',
+    kafka_group_name = 'clickhouse_group',
+    kafka_format = 'CSV';
